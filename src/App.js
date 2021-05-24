1dd1f3f1e2bb327f10ff61ef22ad541dc61e68eb
VasilyBrin <mail@profitdata.ru>
2021-05-24 09:22:11 +0300

    dynamic classes

--- src/App.js
+++ src/App.js
@@ -1,75 +1,79 @@
 import React, { Component } from 'react';
 import './App.css';
-import Post from './components/Post/Post'
+import Post from './components/Post/Post';
+
 
 class App extends Component {
   state = {
     posts: [
-      {name: 'Basil', year: 1957},
-      {name: 'Kate', year: 1996},
-      {name: 'Alex', year: 1986}
+      { name: 'Basil', year: 1957 },
+      { name: 'Kate', year: 1996 },
+      { name: 'Alex', year: 1986 },
     ],
     pageTitle: 'React components',
-    showPosts: false
-  }
+    showPosts: false,
+  };
 
   togglePostsHandler = () => {
-    this.setState ({
-      showPosts: !this.state.showPosts
-    })
-  }
+    this.setState({
+      showPosts: !this.state.showPosts,
+    });
+  };
 
   onChangeName(name, index) {
-    const post = this.state.posts[index]
-    post.name = name
-    const posts = [...this.state.posts]
-    posts[index] = post
-    this.setState({posts})
+    const post = this.state.posts[index];
+    post.name = name;
+    const posts = [...this.state.posts];
+    posts[index] = post;
+    this.setState({ posts });
   }
 
   deleteHandler(index) {
-    const posts = this.state.posts.concat()
-    posts.splice(index, 1)
+    const posts = this.state.posts.concat();
+    posts.splice(index, 1);
 
-    this.setState({posts})
+    this.setState({ posts });
   }
 
-  render () {
+  render() {
     const divStyle = {
-      textAlign: 'center'
-    }
+      textAlign: 'center',
+    };
 
-    let posts = null
+    let posts = null;
 
     if (this.state.showPosts) {
       posts = this.state.posts.map((post, index) => {
         return (
-        <Post
-          key={index}
-          name={post.name}
-          year={post.year}
-          onDelete={this.deleteHandler.bind(this, index)}
-          onChangeName={event => this.onChangeName(event.target.value, index)} 
-        />
-        )
-      })
+          <Post
+            key={index}
+            name={post.name}
+            year={post.year}
+            onDelete={this.deleteHandler.bind(this, index)}
+            onChangeName={(event) =>
+              this.onChangeName(event.target.value, index)
+            }
+          />
+        );
+      });
     }
 
     return (
       <div style={divStyle}>
         <h1>{this.state.pageTitle}</h1>
 
-        <button onClick={this.togglePostsHandler}
-        >Toggle posts</button>
-        <div style={{
-          width: 400,
-          margin: 'auto',
-          paddingTop: '20px'
-        }}>
-          { posts }
-          </div>
+        <button onClick={this.togglePostsHandler}>Toggle posts</button>
+        <div
+          style={{
+            width: 400,
+            margin: 'auto',
+            paddingTop: '20px',
+          }}
+        >
+          { posts } 
         </div>
-     );
+      </div>
+    );
   }
 }
 
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,13 +1,30 @@
 import React from 'react'
-import './Post.css'
+import './Post.css';
 
-const Post = (props) => (
-    <div className="Post">
-        <h3>Post name: {props.name}</h3>
-        <p>Year: <strong>{props.year}</strong></p>
-        <input type="text" onChange={props.onChangeName} value={props.name}/>
-        <button onClick={props.onDelete}>Delete</button>
-    </div>
-)
+const Post = (props) => {
+    const inputClasses = ['input']
+
+    if (props.name !=='') {
+        inputClasses.push('green')
+    } else {
+        inputClasses.push('red')
+    }
+
+    if (props.name.lenght > 4) { 
+        inputClasses.push('bold')
+    }
+    return (
+        <div className="Post">
+            <h3>Post name: {props.name}</h3>
+            <p>Year: <strong>{props.year}</strong></p>
+            <input type="text" 
+                onChange={props.onChangeName} 
+                value={props.name}
+                className={inputClasses.join(' ')}
+            />
+            <button onClick={props.onDelete}>Delete</button>
+        </div>
+    )
+}
 
 export default Post;
